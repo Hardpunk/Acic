be06cfc88d8336d1345a9de9587b462643300997
Bruno <brunospoladore@hotmail.com>
2021-08-09 22:16:13 -0300

    Site views

---
+++ resources/views/includes/_header-carousel.blade.php
@@ -0,0 +1,35 @@
+<section class="mt-0 py-5 mb-6 bg-banner-fixo">
+    <div class="container-fluid px-lg-5">
+        <div class="row align-items-center py-lg-5">
+            <div class="col-12">
+                <div class="row py-5 pl-lg-5">
+                    <div class="col-12 ">
+                        <h1 class="color-white">
+                            Somos a sua plataforma <br>
+                            de aprendizagem contínua
+                        </h1>
+                        <p class="texto-banner">A busca pelo conhecimento<br>
+                            é o primeiro passo...
+                        </p>
+                    </div>
+                    <div class="col-sm-9 col-md-7 col-lg-6 col-xl-4">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <a href="/para-empresas#contato-empresas"
+                                    class="btn btn-block btn-md btn-read-more font-weight-bold waves-effect waves-light m-0 btn-banner-1">
+                                    Capacite a sua equipe
+                                </a>
+                            </div>
+                            <div class="col-sm-6 mt-3 mt-sm-0">
+                                <a href="/#planos"
+                                    class="btn btn-block btn-md btn-read-more font-weight-bold waves-effect waves-light m-0 btn-banner-2">
+                                    Planos de aprendizagem
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
